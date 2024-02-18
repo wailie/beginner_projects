f9a5b0ec371c34402808d48acc5a3eb418d1a759
Zin Wai Htut <126778400+wailie@users.noreply.github.com>
2024-02-18 13:19:30 +0630

    Update scoreboard.py - reset function
    - score reset and record high score

--- Snake_game/scoreboard.py
+++ Snake_game/scoreboard.py
@@ -1,20 +1,35 @@
 from turtle import Turtle
-
 ALIGNMENT = "center"
-FONT = ('Courier', 15, 'normal')
+FONT = ("Courier", 20, "bold")
+
 
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
-        self.hideturtle()
-        self.pencolor("white")
+        self.score = 0
+        self.high_score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.color("white")
         self.penup()
-        self.goto(0, 275)
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def display_score(self, score):
-        self.write(arg=f"Score: {score}", move=False, align=ALIGNMENT, font=FONT)
-    
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg="GAME OVER!", move=False, align=ALIGNMENT, font=FONT)
-    
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > int(self.high_score):
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
+            with open("data.txt", mode="r") as file:
+                self.high_score = int(file.read())   
+        self.score = 0
+        self.update_scoreboard()
