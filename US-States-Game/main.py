ed7789d5b770960b07ef1ac6a3cccda74373fa88
Zin Wai Htut <126778400+wailie@users.noreply.github.com>
2024-02-25 17:38:03 +0630

    Update main.py
    this version provides list of states in .csv format when you missed them.

--- US-States-Game/main.py
+++ US-States-Game/main.py
@@ -31,14 +31,30 @@ turtle.shape(img)
 data = pandas.read_csv("50_states.csv")
 
 #states
-states = data.state
+states = data.state.to_list()
 correct_states = []
+missing_states = states
 
-while not len(correct_states) == 50:
-    answer = screen.textinput(title=f"Guess the State {len(correct_states)}/50", prompt="What's the state's name?").title()
+while len(correct_states) != 50:
+    answer = screen.textinput(title=f"{len(correct_states)}/50 States", prompt="What's the state's name?").title()
+    if answer == "Exit":
+        break
     if check_answer(states, answer):
         correct_states.append(answer)
+        #getting answer row in dataframe
         correct_state = data[data.state == answer]
         write_state(answer, int(correct_state.x.iloc[0]), int(correct_state.y.iloc[0]))
+        missing_states.remove(answer)
+
+
+# states to learn.csv
+# save the missing states to a .csv
+
+missing_states_table = {
+    "States you need to learn": missing_states
+}
+table_data = pandas.DataFrame(missing_states_table)
+table_data.to_csv("missing_states.csv")
 
 screen.exitonclick()
+
